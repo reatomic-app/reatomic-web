20a3cc42aac01e9983710b3261280a4b358afcdb
Mario Garcia <mario.ggar@gmail.com>
2020-07-15 09:01:46 +0200

    feat(forms): add form submission for all types

--- src/reatomic/projects/domain/index.ts
+++ src/reatomic/projects/domain/index.ts
@@ -3,3 +3,26 @@ export interface Project {
     name: string;
     description: string;
 }
+
+export interface DataSourceInput {
+    type: string;
+    description: string;
+    date: Date;
+    url: string;
+}
+
+export interface FactInput {
+    type: string;
+    description: string;
+    link: string;
+}
+
+export interface ConclusionInput {
+    description: string;
+    url: string;
+}
+
+export interface InsightInput {
+    description: string;
+    url: string;
+}
