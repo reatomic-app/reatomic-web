bae0a1ef668879d20c80b6064f5c1624014f125e
alonso.torres <alonso.torres@kaleidos.net>
2023-12-21 23:40:29 +0100

    fix bug markdown

--- src/views/ExperimentView.vue
+++ src/views/ExperimentView.vue
@@ -13,6 +13,10 @@
  const edit = ref(false);
  const data = ref("");
 
+ if(props.experimentId && store.cardData[props.experimentId]?.data) {
+   data.value = store.cardData[props.experimentId]?.data;
+ }
+
  const documentText = computed({
    get() {
      return data.value;
@@ -22,31 +26,31 @@
    }
  });
 
- watch(
+  watch(
    () => props.experimentId && store.cardData[props.experimentId]?.data,
-   (data) => {
-     if (data) {
-       documentText.value = data
+   (value) => {
+     if (value) {
+       data.value = value;
      }
    }
  );
-
 
  onMounted(() => {
    if (props.id && store.current.state === 'empty') {
      store.fetchProjectDetail(props.id);
    }
- });
 
- if (props.id && props.experimentId) {
-   store.fetchCardData(props.id, props.experimentId);
- }
+   // If the data is already loaded
+   if (props.experimentId && store.cardData[props.experimentId]?.data) {
+     document.value = store.cardData[props.experimentId]?.data;
+   }
+ });
 
  watch(() => props.experimentId, (id) => {
    if (props.id && props.experimentId) {
      store.fetchCardData(props.id, props.experimentId);
    }
- });
+ }, {immediate: true});
 
  function startEdit() {
    edit.value = true;
